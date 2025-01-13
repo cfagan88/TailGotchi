79e7dfd80380191b1a6ac5f3c09374b8ee8fbfc2
Jak R-S <176810031+jakr-s@users.noreply.github.com>
2025-01-13 11:40:57 +0000

    Add Supabase authentication integration and client setup

--- Tailgotchi/src/App.tsx
+++ Tailgotchi/src/App.tsx
@@ -1,14 +1,32 @@
-import "./App.css";
-import Header from "./components/Header";
+import { useState, useEffect } from "react";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { supaClient } from "./client";
 
-function App() {
-  return (
-    <>
-      <div></div>
-      <h1>Tailgotchi</h1>
-      <Header></Header>
-    </>
-  );
+export default function App() {
+  const [session, setSession] = useState(null);
+  useEffect(() => {
+    supaClient.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+    const {
+      data: { subscription },
+    } = supaClient.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  if (!session) {
+    return (
+      <Auth
+        supabaseClient={supaClient}
+        providers={["google"]}
+        appearance={{ theme: ThemeSupa }}
+      />
+    );
+  } else {
+    return <div>Logged in!</div>; // Route to Homepage here
+  }
 }
-
-export default App;

6e1554604655ec3a2ab158809a0bc64478c5635c
Moldovia-Peach <hughes.mollyann@outlook.com>
2025-01-13 10:28:44 +0000

    Initial front-end setup completed - added components folder

---
+++ Tailgotchi/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <div></div>
+      <h1>Tailgotchi</h1>
+      <Header></Header>
+    </>
+  );
+}
+
+export default App;

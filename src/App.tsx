cd7beb5db6029a971396374519df9369b1805c89
Jak R-S <176810031+jakr-s@users.noreply.github.com>
2025-01-14 14:51:21 +0000

    Refactor session management into a custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { useState, useEffect } from "react";
-import { Session } from "@supabase/supabase-js";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
 import { supaClient } from "./client";
@@ -14,22 +12,10 @@ import Footer from "./components/Footer";
 import AboutUs from "./components/AboutUs";
 import Home from "./components/Home";
 import ProfileCreation from "./components/ProfileCreation";
+import useSession from "./hooks/useSession";
 
 export default function App() {
-  const [session, setSession] = useState<Session | null>(null);
-
-  useEffect(() => {
-    supaClient.auth.getSession().then(({ data: { session } }) => {
-      setSession(session);
-    });
-    const {
-      data: { subscription },
-    } = supaClient.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
-
-    return () => subscription.unsubscribe();
-  }, []);
+  const session = useSession();
 
   return (
     <Router>
@@ -60,7 +46,7 @@ export default function App() {
             path="/about"
             element={session ? <AboutUs /> : <Navigate to="/" />}
           />
-          <Route 
+          <Route
             path="/profile-creation"
             element={session ? <ProfileCreation /> : <Navigate to="/" />}
           />

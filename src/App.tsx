ff581b8019e2519e60099239e812a4f20c8d88c8
Jak R-S <176810031+jakr-s@users.noreply.github.com>
2025-01-14 11:36:32 +0000

    feat: integrate react-router for navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,29 @@
-import { useState, useEffect } from "react";
-import { Session } from "@supabase/supabase-js";
+import { useEffect } from "react";
 import { supaClient } from "./api/client";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import CreateProfile from "./components/CreateProfile";
+import { Session } from "@supabase/supabase-js";
 import AuthWrapper from "./components/AuthWrapper";
 
 export default function App() {
-  const [session, setSession] = useState<Session | null>(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    supaClient.auth.getSession().then(({ data: { session } }) => {
-      setSession(session);
-    });
+    const handleAuthChange = (_event: string, session: Session | null) => {
+      navigate(session ? "/create-profile" : "/login");
+    };
+
     const {
       data: { subscription },
-    } = supaClient.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
+    } = supaClient.auth.onAuthStateChange(handleAuthChange);
 
     return () => subscription.unsubscribe();
-  }, []);
+  }, [navigate]);
 
-  if (!session) {
-    return <AuthWrapper />;
-  } else {
-    return <CreateProfile />;
-  }
+  return (
+    <Routes>
+      <Route path="/create-profile" element={<CreateProfile />} />
+      <Route path="/login" element={<AuthWrapper />} />
+    </Routes>
+  );
 }

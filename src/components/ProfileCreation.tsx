8a55ef535569560ae995c0a441b2cc24f51f89c2
Jak R-S <176810031+jakr-s@users.noreply.github.com>
2025-01-14 14:17:13 +0000

    Refactor ProfileCreation component handle profile submission to backend

--- src/components/ProfileCreation.tsx
+++ src/components/ProfileCreation.tsx
@@ -1,22 +1,74 @@
-const ProfileCreation = () => {
-    return(
-       
-        <main className="bg-primarylight flex flex-col space-y-4 justify-center items-center m-auto w-screen h-[70vh]">
-          <h1 className="font-jersey25 text-navy">Profile Creation</h1>
-          <div className="rounded-md border-4 border-mediumblue bg-lightblue shadow shadow-navy h-[50%] flex items-center">
-            <form className="m-auto flex flex-col space-y-4">
-              <label>Username:</label>
-              <input type="text" className="bg-primarylight"></input>
-              <label>First Name:</label>
-              <input type="text" className="bg-primarylight"></input>
-              <label>Last Name:</label>
-              <input type="text" className="bg-primarylight"></input>
-              <input type="file"></input>
-            </form>
-          </div>
-        </main>
-       
-    )
-}
+import { useState } from "react";
+import { supaClient } from "../client";
+import { useNavigate } from "react-router-dom";
 
-export default ProfileCreation;
+export default function CreateProfile() {
+  const [username, setUsername] = useState("");
+  const [name, setName] = useState("");
+  const [avatarUrl, setAvatarUrl] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const navigate = useNavigate();
+    const {
+      data: { user },
+    } = await supaClient.auth.getUser();
+
+    console.log(user);
+
+    if (!user) {
+      // Do something
+      return;
+    }
+
+    try {
+      const { data } = await supaClient.from("users_profiles").insert([
+        {
+          user_id: user.id,
+          username: username,
+          name: name,
+          avatar_url: avatarUrl,
+        },
+      ]);
+
+      if (data) {
+        navigate("/home");
+        console.log(data);
+      }
+    } catch (error) {
+      // Do something
+      console.log(error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username (required)
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        Name (optional)
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <label>
+        Avatar URL (optional)
+        <input
+          type="text"
+          value={avatarUrl}
+          onChange={(e) => setAvatarUrl(e.target.value)}
+        />
+      </label>
+      <button type="submit">Create Profile</button>
+    </form>
+  );
+}

ba4b6c477b5eb41b3d758186b1b82388d8d9ee07
Jak R-S <176810031+jakr-s@users.noreply.github.com>
2025-01-15 13:28:26 +0000

    Refactor to use Task type from global types

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,41 +1,70 @@
-import { useState } from "react"
-import { supaClient } from "../api/client"
+import { useState } from "react";
+import { supaClient } from "../api/client";
+import { Task } from "../api/global.types";
 
-const TaskCard=({task})=>{
-    const [petName,setPetName]=useState<string>("")
-    const {task_id, pet_id, task_name, task_info}=task
-    // need to see if we can find difference between current date and created_at date for task
-    // const testDate=new Date()
-    const handleClick=()=>{
-        //update task to complete if button is pressed
-        supaClient.from('tasks').update({is_completed:true}).eq('task_id',task_id)
-    }
-    supaClient.from('pets').select('pet_name').eq('pet_id',pet_id).then(({data})=>{
-        if(data){
-            setPetName(data[0].pet_name)
-            return
-        }
-    })
+const TaskCard = ({ task }: { task: Task }) => {
+  const {
+    // completed_at,
+    // created_at,
+    // is_completed,
+    pet_id,
+    task_id,
+    task_info,
+    task_name,
+  } = task;
 
-    return <div className="p-8 text-navy rounded-xl my-4 bg-primarylight max-w-4xl mx-auto">
-        <h2 className="font-jersey25 text-navy">Pet Name goes here
-            {/* petName */}
-        </h2>
-        <h3 className="font-jersey25 text-navy">Task name goes here
-            {/* task_name */}
-        </h3>
-        <p className="font-jersey25 text-navy">More info about the tasks
-            {/* task_info */}
-        </p>
-        {/*check with others which they prefer, checkbox or complete button */}
-        <label className="font-jersey25 text-navy">
+  const [petName, setPetName] = useState<string>("");
+
+  //   need to see if we can find difference between current date and created_at date for task
+  //   const testDate=new Date()
+
+  //update task to complete if button is pressed
+  const handleClick = () => {
+    supaClient
+      .from("tasks")
+      .update({ is_completed: true })
+      .eq("task_id", task_id);
+  };
+
+  supaClient
+    .from("pets")
+    .select("pet_name")
+    .eq("pet_id", pet_id)
+    .then(({ data }) => {
+      if (data) {
+        setPetName(data[0].pet_name);
+        return;
+      }
+    });
+
+  return (
+    <div className="p-8 text-navy rounded-xl my-4 bg-primarylight max-w-4xl mx-auto">
+      <h2 className="font-jersey25 text-navy">
+        Pet Name goes here
+        {petName}
+      </h2>
+      <h3 className="font-jersey25 text-navy">
+        Task name goes here
+        {task_name}
+      </h3>
+      <p className="font-jersey25 text-navy">
+        More info about the tasks
+        {task_info}
+      </p>
+      {/*check with others which they prefer, checkbox or complete button */}
+      <label className="font-jersey25 text-navy">
         Complete:
         <input type="checkbox"></input>
-        </label>
-        <button type="button" onClick={handleClick} className="font-jersey25 text-navy mb-4">Complete</button>
+      </label>
+      <button
+        type="button"
+        onClick={handleClick}
+        className="font-jersey25 text-navy mb-4"
+      >
+        Complete
+      </button>
     </div>
+  );
+};
 
-}
-
-
-export default TaskCard
+export default TaskCard;

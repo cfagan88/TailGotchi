d1abf112ed38b595cc2061096105a5e20c2c5965
Jak R-S <176810031+jakr-s@users.noreply.github.com>
2025-01-15 13:07:51 +0000

    Add Pet and UserProfile types

--- src/api/global.types.ts
+++ src/api/global.types.ts
@@ -1,3 +1,7 @@
 import { Database } from "./database.types";
 
 export type Task = Database["public"]["Tables"]["tasks"]["Row"];
+
+export type Pet = Database["public"]["Tables"]["pets"]["Row"];
+
+export type UserProfile = Database["public"]["Tables"]["users_profiles"]["Row"];

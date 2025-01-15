155232cae60a4c8742c1783ad2fcc719b44db989
Chris Fagan <cfagan88@hotmail.co.uk>
2025-01-15 11:47:55 +0000

    added add pet functionality

--- src/components/AddPet.tsx
+++ src/components/AddPet.tsx
@@ -1,9 +1,56 @@
+import { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+import { supaClient } from "../api/client";
 import dogNodding from "../assets/animations and images/dog-nodding.gif";
 import bouncingFullHeart from "../assets/animations and images/bouncing-full-heart.gif";
 import bouncingEmptyHeart from "../assets/animations and images/bouncing-grey-heart.gif";
 import bouncingStar from "../assets/animations and images/bouncing-star.gif";
 
 const AddPet = () => {
+  const [petName, setPetName] = useState<string>("");
+  const [petAge, setPetAge] = useState<number | null>(null);
+  const [breed, setBreed] = useState<string | null>(null);
+  const [gender, setGender] = useState<string | null>(null);
+  const [likes, setLikes] = useState<string | null>(null);
+  const [dislikes, setDislikes] = useState<string | null>(null);
+  const [petCareInfo, setPetCareInfo] = useState<string | null>(null);
+  const [formError, setFormError] = useState<string | null>(null);
+  // const navigate = useNavigate();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!petName) {
+      setFormError("Please fill in all required fields");
+      return;
+    }
+
+    try {
+      const { data } = await supaClient
+        .from("pets")
+        .insert([
+          {
+            pet_name: petName,
+            pet_age: petAge,
+            breed: breed,
+            gender: gender,
+            pet_likes: likes,
+            pet_dislikes: dislikes,
+            pet_care_info: petCareInfo,
+          },
+        ])
+        .select();
+      if (data) {
+        console.log(data);
+        setFormError(null);
+        // navigate("/"); go to pet profile page once created
+      }
+    } catch (error) {
+      setFormError("Error in adding pet");
+      console.log(error);
+    }
+  };
+
   return (
     <div className="p-8 text-navy rounded-xl my-4 bg-primarylight max-w-4xl mx-auto">
       <div className="flex mb-2">
@@ -31,6 +78,7 @@ const AddPet = () => {
               required
               className="w-full p-2 mt-1 border border-mediumblue rounded"
               placeholder="What is your pet's name?"
+              onChange={(e) => setPetName(e.target.value)}
             />
           </div>
           <div className="flex-1">
@@ -38,10 +86,13 @@ const AddPet = () => {
               Age
             </label>
             <input
-              type="text"
+              type="number"
               id="pet-age"
+              min={0}
+              max={25}
               className="w-full p-2 mt-1 border border-mediumblue rounded"
               placeholder="How old is your pet?"
+              onChange={(e) => setPetAge(e.target.value)}
             />
           </div>
         </div>
@@ -56,6 +107,7 @@ const AddPet = () => {
               id="pet-breed"
               className="w-full p-2 mt-1 border border-mediumblue rounded"
               placeholder="What breed is your pet?"
+              onChange={(e) => setBreed(e.target.value)}
             />
           </div>
           <div className="flex-1">
@@ -67,6 +119,7 @@ const AddPet = () => {
               id="pet-gender"
               className="w-full p-2 mt-1 border border-mediumblue rounded"
               placeholder="What is your pet's gender?"
+              onChange={(e) => setGender(e.target.value)}
             />
           </div>
         </div>
@@ -85,6 +138,7 @@ const AddPet = () => {
           id="pet-likes"
           className="w-full p-2 mt-1 border border-mediumblue rounded"
           placeholder="What activities or toys does your pet enjoy?"
+          onChange={(e) => setLikes(e.target.value)}
         />
 
         <div className="flex items-center space-x-1 mb-4">
@@ -101,6 +155,7 @@ const AddPet = () => {
           id="pet-dislikes"
           className="w-full p-2 mt-1 border border-mediumblue rounded"
           placeholder="Is there anything your pet dislikes?"
+          onChange={(e) => setDislikes(e.target.value)}
         />
 
         <div className="flex items-center space-x-1 mb-4">
@@ -117,10 +172,15 @@ const AddPet = () => {
           id="pet-care"
           className="w-full p-2 mt-1 border border-mediumblue rounded"
           placeholder="Does your pet require any special care or attention?"
+          onChange={(e) => setPetCareInfo(e.target.value)}
         />
 
         <div className="flex justify-center mt-4">
-          <button className="bg-lightblue px-20 py-2 rounded-full font-extrabold text-white hover:bg-mediumblue border-solid border-mediumblue border-b-4 border-r-2 hover:border-lightblue">
+          <button
+            onClick={handleSubmit}
+            type="submit"
+            className="bg-lightblue px-20 py-2 rounded-full font-extrabold text-white hover:bg-mediumblue border-solid border-mediumblue border-b-4 border-r-2 hover:border-lightblue"
+          >
             Add Pet
           </button>
         </div>

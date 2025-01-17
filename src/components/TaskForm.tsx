6382e2b0643f962e8ab3e49d6385ef3f491cc952
AndrewLau97 <andrewlau1997@hotmail.co.uk>
2025-01-17 15:19:33 +0000

    style task form component

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -11,11 +11,6 @@ const TaskForm: React.FC<ComponentProps> = ({ setShowForm }) => {
   const [taskName, setTaskName] = useState<string>("");
   const [taskInfo, setTaskInfo] = useState<string>("");
   const [selectedPet, setSelectedPet] = useState<string>("");
-  // const [required,setRequired]=useState({
-  //   taskName:null as string|null,
-  //   pet_id:null as number|null,
-  //   CompletionDate: null as number|null,
-  // })
 
   useEffect(() => {
     const getData = async () => {
@@ -27,14 +22,13 @@ const TaskForm: React.FC<ComponentProps> = ({ setShowForm }) => {
           .from("pets")
           .select("*,users_pets!inner(*)")
           .eq("users_pets.user_id", user.id);
-        //   const {data}=await supaClient.from('pets').select('*,users_pets!inner(*))').eq('pets.users_pets.user_id',user.id)
         setMyPets(data);
       }
     };
     getData();
   }, []);
 
-  const handleAddTask = async (e: React.FormEvent<HTMLButtonElement>) => {
+  const handleAddTask = async (e: React.FormEvent) => {
     e.preventDefault();
     if (!taskName || !selectedPet) {
       console.log("error", taskName, selectedPet);
@@ -63,45 +57,59 @@ const TaskForm: React.FC<ComponentProps> = ({ setShowForm }) => {
     }
   };
   return (
-    <form className="p-8 text-navy rounded-xl my-4 bg-primarylight max-w-4xl mx-auto">
-      <h2>Add a task</h2>
-      <input
-        type="text"
-        placeholder="Task Name"
-        required
-        value={taskName}
-        onChange={(e) => setTaskName(e.target.value)}
-        className="mb-4 bg-white"
-      />
-      <input
-        type="text"
-        placeholder="Task Description"
-        required
-        value={taskInfo}
-        onChange={(e) => setTaskInfo(e.target.value)}
-        className="mb-4 bg-white"
-      />
-      <label>Assign Pet</label>
-      <select
-        required
-        className="mb-4 bg-white"
-        onChange={(e) => {
-          setSelectedPet(e.target.value);
-        }}
-        defaultValue={"placeholder"}
-      >
-        <option value={"placeholder"} hidden disabled>
-          Please Select a Pet
-        </option>
-        {myPets?.map((pet) => {
-          return (
-            <option key={pet.pet_name} value={pet.pet_id}>
-              {pet.pet_name}
-            </option>
-          );
-        })}
-      </select>
-      <button onClick={handleAddTask}>Add Task</button>
+    <form
+      className="p-8 text-navy rounded-xl my-4 bg-primarylight max-w-4xl mx-auto"
+      onSubmit={handleAddTask}
+    >
+      <div className="flex space-x-4">
+        <div className="flex-1">
+          <h2 className="text-h2 font-jersey25">Create task</h2>
+          <input
+            type="text"
+            placeholder="Task Name"
+            required
+            value={taskName}
+            onChange={(e) => setTaskName(e.target.value)}
+            className={`w-full p-2 mt-1 border ${
+              taskName ? "border-mediumblue" : "border-red-500"
+            } rounded bg-white text-navy`}
+          />
+          <input
+            type="text"
+            placeholder="Task Description"
+            value={taskInfo}
+            onChange={(e) => setTaskInfo(e.target.value)}
+            className={`w-full p-2 mt-1 border rounded navy bg-white text-navy`}
+          />
+          <label htmlFor="pet_id" className="text-h2 font-jersey25">Assign Pet </label>
+          <br />
+          <div className="flex">
+            <select
+              required
+              className={`w-full p-2  border ${
+                selectedPet ? "border-mediumblue" : "border-red-500"
+              } rounded bg-white text-navy`}
+              onChange={(e) => {
+                setSelectedPet(e.target.value);
+              }}
+              defaultValue={""}
+              id='pet_id'
+            >
+              <option value={""} hidden disabled>
+                Please Select a Pet
+              </option>
+              {myPets?.map((pet) => {
+                return (
+                  <option key={pet.pet_name} value={pet.pet_id}>
+                    {pet.pet_name}
+                  </option>
+                );
+              })}
+            </select>
+            <button className="float-right bg-lightblue hover:bg-mediumblue text-white font-bold py-2 px-4 rounded-2xl w-64 ms-3">Add Task</button>
+          </div>
+        </div>
+      </div>
     </form>
   );
 };

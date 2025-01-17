510be85bb3ec9a981fd94ede4f29bfd12555e972
Molly Hughes <hughes.mollyann@outlook.com>
2025-01-17 17:03:05 +0000

    Resolved issues with navbar (#59)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import NavBar from "./NavBar";
 
 const Header = () => {
   return (
-    <header className="flex items-center p-4 sm:p-6 bg-navy text-lightblue flex-wrap">
+    <header className="flex items-center p-4 sm:p-6 bg-navy text-lightblue flex-wrap w-screen">
       <h1 className="text-2xl sm:text-4xl font-extrabold flex-1 text-left">
         TailGotchi
       </h1>

a7d46c6b1f994ffcfedd63a5b0130bf8fb7d4c7e
AndrewLau97 <andrewlau1997@hotmail.co.uk>
2025-01-20 16:46:28 +0000

    add assign user to add task form

--- src/api/global.types.ts
+++ src/api/global.types.ts
@@ -5,3 +5,5 @@ export type Task = Database["public"]["Tables"]["tasks"]["Row"];
 export type Pet = Database["public"]["Tables"]["pets"]["Row"];
 
 export type UserProfile = Database["public"]["Tables"]["users_profiles"]["Row"];
+
+export type UsersPets = Database["public"]["Tables"]["users_pets"]["Row"]

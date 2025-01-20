13a0361ebdf5582e25dfb9c4a2e20fddd73016ec
Jak R-S <176810031+jakr-s@users.noreply.github.com>
2025-01-20 04:52:31 +0000

    fix: correct typo and change TimeFormat interface to type

--- src/utils/convertMilliseconds.ts
+++ src/utils/convertMilliseconds.ts
@@ -1,14 +1,14 @@
-interface TimeFormat {
+type TimeFormat = {
   d: number;
   h: number;
   m: number;
   s: number;
-}
+};
 
 /**
  * Converts milliseconds into various time formats.
  *
- * @param {number} miliseconds - The number of milliseconds to convert.
+ * @param {number} milliseconds - The number of milliseconds to convert.
  * @param {"s" | "m" | "h" | "d" | undefined} format - The format to convert to:
  *   - "s" for total seconds
  *   - "m" for total minutes
@@ -18,20 +18,20 @@ interface TimeFormat {
  * @returns {number | TimeFormat} The converted time in the specified format, or an object with days, hours, minutes, and seconds if no format is specified.
  */
 
-export default function convertMiliseconds(
-  miliseconds: number,
+export default function convertMilliseconds(
+  milliseconds: number,
   format: "s" | "m" | "h" | "d" | undefined
 ): number | TimeFormat {
   let days, hours, minutes, seconds, total_hours, total_minutes, total_seconds;
 
-  total_seconds = Number(Math.floor(miliseconds / 1000));
-  total_minutes = Number(Math.floor(total_seconds / 60));
-  total_hours = Number(Math.floor(total_minutes / 60));
-  days = Number(Math.floor(total_hours / 24));
+  total_seconds = Math.floor(milliseconds / 1000);
+  total_minutes = Math.floor(total_seconds / 60);
+  total_hours = Math.floor(total_minutes / 60);
+  days = Math.floor(total_hours / 24);
 
-  seconds = Number(total_seconds % 60);
-  minutes = Number(total_minutes % 60);
-  hours = Number(total_hours % 24);
+  seconds = total_seconds % 60;
+  minutes = total_minutes % 60;
+  hours = total_hours % 24;
 
   switch (format) {
     case "s":
